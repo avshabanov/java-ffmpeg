c6504d4a97a1605202d7cac33eae34318da10e8d
Alexander Shabanov <avshabanov@gmail.com>
2012-08-17 00:15:12 +0400

    Initial import

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ java-ffmpeg
 ===========
 
 Java media library - abstraction layer for ffmpeg.
+Inspired by fmj, but created in hope to produce small yet powerfull modular multimedia library.
+
---
+++ src/mm4j-av-native/src/main/java/com/alexshabanov/mm4j/App.java
@@ -0,0 +1,10 @@
+package com.alexshabanov.mm4j;
+
+/**
+ * Entry point.
+ */
+public final class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
